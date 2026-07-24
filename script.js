98bf90aecbe2457ac635bf25e4b65f3f8312c02d
alexisdanoizelpro-dev <alexis.danoizel.pro@outlook.fr>
2026-07-24 11:51:06 +0200

    La Porte révèle sa Lumière

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 document.addEventListener("DOMContentLoaded", () => {
+  const seuil = document.querySelector(".seuil");
+  const porte = document.querySelector(".porte");
 
-    const porte = document.querySelector(".porte");
+  if (!seuil || !porte) {
+    return;
+  }
 
-    porte.addEventListener("click", () => {
-
-        porte.classList.add("ouverte");
-
-    });
-
+  porte.addEventListener("click", () => {
+    porte.classList.add("ouverte");
+    seuil.classList.add("entree-ouverte");
+  });
 });
