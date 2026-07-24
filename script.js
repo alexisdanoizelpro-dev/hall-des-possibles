e0cf6ca3d0d5330286b1c60012a632adc3fef80b
alexisdanoizelpro-dev <alexis.danoizel.pro@outlook.fr>
2026-07-24 11:33:40 +0200

    Naissance du Coeur du Hall

---
+++ script.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+    const porte = document.querySelector(".porte");
+
+    porte.addEventListener("click", () => {
+
+        porte.classList.add("ouverte");
+
+    });
+
+});
