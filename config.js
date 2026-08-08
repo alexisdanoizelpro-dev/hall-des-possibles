a6396108a34be5a53264616a13849a78b5822011
alexisdanoizelpro-dev <alexis.danoizel.pro@outlook.fr>
2026-08-08 11:56:25 +0200

    Add files via upload

---
+++ config.js
@@ -0,0 +1,10 @@
+// ==========================================================
+// LE HALL DES POSSIBLES — V0.6.1
+// Configuration simple du Hall
+// ==========================================================
+
+window.CONFIG_SEUIL = {
+  // true  = la Coupelle de Lait est disponible
+  // false = la Coupelle de Lait disparaît complètement
+  coupelleDisponible: true
+};
