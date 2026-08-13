b9268b5a5eb73821111400b461899d82fcb67e8b
alexisdanoizelpro-dev <alexis.danoizel.pro@outlook.fr>
2026-08-13 06:50:17 +0200

    Update script-v1.js

--- script-v1.js
+++ script-v1.js
@@ -16,3 +16,24 @@ L'essentiel est suffisant.
 "use strict";
 
 const Registre = window.RegistreDuSeuil;
+
+/*
+==========================================================
+
+LE GARDIEN
+
+Le Gardien veille sur le Hall.
+
+Il ne décide jamais.
+
+Il écoute le Registre.
+Il donne simplement vie au Hall.
+
+L'essentiel est suffisant.
+
+==========================================================
+*/
+
+"use strict";
+
+const Registre = window.RegistreDuSeuil;
