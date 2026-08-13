f0bc09f3d47c50a7a333cabb0311316b2e7ade4a
alexisdanoizelpro-dev <alexis.danoizel.pro@outlook.fr>
2026-08-13 06:52:12 +0200

    Update script-v1.js

--- script-v1.js
+++ script-v1.js
@@ -1,25 +1,6 @@
 /*
 ==========================================================
 
-LE HALL DES POSSIBLES
-
-Ce fichier écoute le Registre.
-
-Il ne décide jamais.
-Il donne simplement vie au Hall.
-
-L'essentiel est suffisant.
-
-==========================================================
-*/
-
-"use strict";
-
-const Registre = window.RegistreDuSeuil;
-
-/*
-==========================================================
-
 LE GARDIEN
 
 Le Gardien veille sur le Hall.
