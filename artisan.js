eab528bc7848f96cd8b211da64d04d72ccf0364c
alexisdanoizelpro-dev <alexis.danoizel.pro@outlook.fr>
2026-08-21 17:02:24 +0200

    Create artisan.js

---
+++ artisan.js
@@ -0,0 +1,30 @@
+"use strict";
+
+/* =========================================
+   LE PANNEAU DE L'ARTISAN
+   =========================================
+
+   Ce fichier contient uniquement les éléments
+   que l'Artisan peut modifier facilement.
+
+   true  = visible dans le Hall
+   false = absent du Hall
+
+   ========================================= */
+
+
+const ARTISAN = {
+
+    /* -------------------------------------
+       RÉFLEXION DU SEUIL
+       ------------------------------------- */
+
+    reflexion: {
+
+        active: true,
+
+        texte: "La vie avance en silence. Si tu veux vivre, alors fais du bruit..."
+
+    }
+
+};
