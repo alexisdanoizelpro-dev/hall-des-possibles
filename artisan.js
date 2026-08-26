8420a807d46ba4cba24871bb96ab24bc82a035bb
alexisdanoizelpro-dev <alexis.danoizel.pro@outlook.fr>
2026-08-26 13:53:52 +0200

    Update artisan.js

--- artisan.js
+++ artisan.js
@@ -53,7 +53,7 @@ const ARTISAN = {
         // x / y : de 0 à 1. La position reste donc stable sur PC et téléphone.
         position: {
             x: 0.405,
-            y: 0.375,
+            y: 0.360,
             largeur: 0.046
         }
     }
